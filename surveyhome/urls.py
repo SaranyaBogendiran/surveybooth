5301416dc53a85b34a4dc6bb4ca14878b1d66f75
Saranya Bogendiran <bsaranyabogendiran@gmail.com>
2019-06-19 11:13:50 +0530

    index, Login, Sign Up, Contactus - Working

--- surveyhome/urls.py
+++ surveyhome/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
 path('', views.index, name='index'),
 path('login/', views.login_view, name='login'),
 path('register/', views.register, name='register'),
+path('contactus/', views.emailView, name='contactus'),
 path('dashboard/', views.dashboard, name='dashboard'),
 path('', views.logout_view, name='logout'),
 path('passwordreset/', views.password_change, name='password_change'),
@@ -18,5 +19,5 @@ url(r'passwordreset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9
 path('email/', views.emailView, name='email'),
 path('success/', views.successView, name='success'),
 url(r'^ajax/validate_registerform/$', views.validate_registerform, name='validate_registerform'),
-url(r'^ajax/login_validation/$', views.login_validation, name='login_validation'),
+# url(r'^ajax/login_validation/$', views.login_validation, name='login_validation'),
 ]
